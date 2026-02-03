d62e17c8712d115fa01cbcbb5ead8b7a2099a694
Yuval Tessone <yuval@zebrapps.com>
2026-02-04 00:33:48 +0200

    revert  price to 2350

--- src/main.js
+++ src/main.js
@@ -456,7 +456,7 @@ async function sendRegistrationEmail(data) {
                         gtag('event', 'begin_checkout', {
                             event_category: 'ecommerce',
                             event_label: 'Redirect to Payment',
-                            value: 3000
+                            value: 2350
                         });
                     }
 
